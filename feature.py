37126d9751e78e3cc882db7e22f1ef11dbf6a704
Bhavya <your-bhavyaavula13@gmail.com>
2026-08-12 18:27:48 +0530

    Added calculator feature

---
+++ feature.py
@@ -0,0 +1,11 @@
+
+def calculate_sum(a, b):
+    return a + b
+
+
+def calculate_product(a, b):
+    return a * b
+
+
+print("Sum:", calculate_sum(10, 5))
+print("Product:", calculate_product(10, 5))
